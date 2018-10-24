7db03e1498206bcdc6b8903d402b390a2df220fd
bingyunsky <42928786+bingyunsky@users.noreply.github.com>
2018-10-24 17:53:07 +0800

    Add files via upload

---
+++ content_management_system/users/new_manage/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+from DjangoUeditor import urls as DjangoUeditor_urls
+from django.conf import settings
+
+urlpatterns = [
+	url(r'^admin/', include(admin.site.urls)),
+	url(r'^ueditor/', include(DjangoUeditor_urls))
+]
+
+
+if settings.DEBUG:
+	form django.conf.urls.static import static
+	urlpatterns +=static(
+		settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
